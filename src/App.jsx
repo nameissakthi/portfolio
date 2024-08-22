8c9da5702137044df4debb3e00b4282016627539
nameissakthi <svel7252@gmail.com>
2024-08-22 23:32:37 +0530

    Navbar error updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from './components/About/About'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
 import Sidebar from './components/Sidebar/Sidebar'
 
